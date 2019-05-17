17ee89775a287a9c2f18b2cf61abce59ceb26c44
IJuanTM <iwan2510@gmail.com>
2019-05-17 14:06:59 +0200

    moet je ontdekken

--- index.php
+++ index.php
@@ -4,3 +4,5 @@ echo "Dit is nieuwe code voor de tweede commit.<br>";
 echo "Dit is de derde commit.<br>";
 echo "Dit is de zoveelste commit.<br>";
 echo "Dit is nog een commit.<br>";
+echo "Nog zo eentje hier.";
+echo "Nog een?! Had ik niet verwacht!";

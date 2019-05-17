b62789f4f1914e161ef71223cb8929253e99f140
IJuanTM <iwan2510@gmail.com>
2019-05-17 14:16:35 +0200

    WOOOOOOOOOOOOOOOOOOOOOOOOOOOOWWWWWWWWWWWW

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ echo "Dit is nieuwe code voor de tweede commit.<br>";
 echo "Dit is de derde commit.<br>";
 echo "Dit is de zoveelste commit.<br>";
 echo "Dit is nog een commit.<br>";
-echo "Nog zo eentje hier.";
-echo "Nog een?! Had ik niet verwacht!";
+echo "Nog zo eentje hier.<br>";
+echo "Nog een?! Had ik niet verwacht!<br>";

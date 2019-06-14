97a4b404593af3216e443529d5adc966fcee4e73
IJuanTM <iwan2510@gmail.com>
2019-06-14 13:01:32 +0200

    help

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Hello Git World!<br>";
-echo "Dit is nieuwe code voor de tweede commit.<br>";
-echo "Dit is de derde commit.<br>";
-echo "Dit is de zoveelste commit.<br>";
-echo "Dit is nog een commit.<br>";
-echo "Nog zo eentje hier.<br>";
-echo "Nog een?! Had ik niet verwacht!<br>";
